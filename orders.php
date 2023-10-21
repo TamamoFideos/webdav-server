5a72eb92607d27ee8401dd27dd354450673be434
TamamoFideos <rgremory85@gmail.com>
2023-10-21 00:50:05 -0600

    123

--- orders.php
+++ orders.php
@@ -1,10 +1,98 @@
-<?php
+<?php ?>
 try{
-    include "localhost";
-    
-}
-catch (\Throwable $t) {
-    echo "caught!\n";
-    echo $t->getMessage(), " at ", $t->getFile(), ":", $t->getLine(), "\n";
-}
-?>
+    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+<html>
+
+<head>
+    <title>Index of /</title>
+</head>
+
+<body>
+    <h1>Index of /</h1>
+    <table>
+   	 <tr>
+   		 <th valign="top"><img src="/icons/blank.gif" alt="[ICO]"></th>
+   		 <th><a href="?C=N;O=D">Name</a></th>
+   		 <th><a href="?C=M;O=A">Last modified</a></th>
+   		 <th><a href="?C=S;O=A">Size</a></th>
+   		 <th><a href="?C=D;O=A">Description</a></th>
+   	 </tr>
+   	 <tr>
+   		 <th colspan="5">
+   			 <hr>
+   		 </th>
+   	 </tr>
+   	 <tr>
+   		 <td valign="top"><img src="/icons/unknown.gif" alt="[   ]"></td>
+   		 <td><a href="access.log">access.log</a></td>
+   		 <td align="right">2023-10-21 06:44 </td>
+   		 <td align="right"> 19K</td>
+   		 <td>&nbsp;</td>
+   	 </tr>
+   	 <tr>
+   		 <td valign="top"><img src="/icons/layout.gif" alt="[   ]"></td>
+   		 <td><a href="client_1_13.pdf">client_1_13.pdf</a></td>
+   		 <td align="right">2023-10-21 02:11 </td>
+   		 <td align="right"> 0 </td>
+   		 <td>&nbsp;</td>
+   	 </tr>
+   	 <tr>
+   		 <td valign="top"><img src="/icons/unknown.gif" alt="[   ]"></td>
+   		 <td><a href="cosito.php">cosito.php</a></td>
+   		 <td align="right">2023-10-21 05:21 </td>
+   		 <td align="right">681 </td>
+   		 <td>&nbsp;</td>
+   	 </tr>
+   	 <tr>
+   		 <td valign="top"><img src="/icons/unknown.gif" alt="[   ]"></td>
+   		 <td><a href="cosito.php.save">cosito.php.save</a></td>
+   		 <td align="right">2023-10-21 04:35 </td>
+   		 <td align="right">480 </td>
+   		 <td>&nbsp;</td>
+   	 </tr>
+   	 <tr>
+   		 <td valign="top"><img src="/icons/unknown.gif" alt="[   ]"></td>
+   		 <td><a href="cosito.php.save.1">cosito.php.save.1</a></td>
+   		 <td align="right">2023-10-21 04:35 </td>
+   		 <td align="right">149 </td>
+   		 <td>&nbsp;</td>
+   	 </tr>
+   	 <tr>
+   		 <td valign="top"><img src="/icons/unknown.gif" alt="[   ]"></td>
+   		 <td><a href="error.log">error.log</a></td>
+   		 <td align="right">2023-10-21 06:44 </td>
+   		 <td align="right"> 18K</td>
+   		 <td>&nbsp;</td>
+   	 </tr>
+   	 <tr>
+   		 <td valign="top"><img src="/icons/unknown.gif" alt="[   ]"></td>
+   		 <td><a href="pass.dav">pass.dav</a></td>
+   		 <td align="right">2023-10-03 14:05 </td>
+   		 <td align="right"> 45 </td>
+   		 <td>&nbsp;</td>
+   	 </tr>
+   	 <tr>
+   		 <td valign="top"><img src="/icons/unknown.gif" alt="[   ]"></td>
+   		 <td><a href="prueba.php">prueba.php</a></td>
+   		 <td align="right">2023-10-21 02:30 </td>
+   		 <td align="right"> 20 </td>
+   		 <td>&nbsp;</td>
+   	 </tr>
+   	 <tr>
+   		 <td valign="top"><img src="/icons/folder.gif" alt="[DIR]"></td>
+   		 <td><a href="webdav-server/">webdav-server/</a></td>
+   		 <td align="right">2023-10-21 06:44 </td>
+   		 <td align="right"> - </td>
+   		 <td>&nbsp;</td>
+   	 </tr>
+   	 <tr>
+   		 <th colspan="5">
+   			 <hr>
+   		 </th>
+   	 </tr>
+    </table>
+    <address>Apache/2.4.56 (Debian) Server at 10.0.0.14 Port 80</address>
+</body>
+
+</html>
+
