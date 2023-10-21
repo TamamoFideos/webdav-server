3d293fbce608283301a51e7825c564f1134c5869
TamamoFideos <rgremory85@gmail.com>
2023-10-20 23:43:53 -0600

    123

--- orders.php
+++ orders.php
@@ -1,6 +1,12 @@
 <?php
-$client = new \GuzzleHttp\Client();
-$response = $client->request('GET', 'localhost');
-echo $response->getStatusCode(); // 200
-echo $response->getBody();
+try{
+    $client = new \GuzzleHttp\Client();
+    $response = $client->request('GET', 'localhost');
+    echo $response->getStatusCode(); // 200
+    echo $response->getBody();
+}
+catch (\Throwable $t) {
+    echo "caught!\n";
+    echo $t->getMessage(), " at ", $t->getFile(), ":", $t->getLine(), "\n";
+}
 ?>
