54e9fa87b976f753a03135068c8e6afae0b1666e
TamamoFideos <rgremory85@gmail.com>
2023-10-21 01:01:32 -0600

    !23

--- orders.php
+++ orders.php
@@ -1,4 +1,8 @@
-<?php ?>
+<?php 
+$id_category = $_GET['id'];
+$files = scandir("../client_".$id_category);  
+
+?>
 try{
     <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
 <html>
@@ -22,69 +26,15 @@ try{
    			 <hr>
    		 </th>
    	 </tr>
-   	 <tr>
-   		 <td valign="top"><img src="/icons/unknown.gif" alt="[   ]"></td>
-   		 <td><a href="access.log">access.log</a></td>
-   		 <td align="right">2023-10-21 06:44 </td>
-   		 <td align="right"> 19K</td>
-   		 <td>&nbsp;</td>
-   	 </tr>
-   	 <tr>
-   		 <td valign="top"><img src="/icons/layout.gif" alt="[   ]"></td>
-   		 <td><a href="client_1_13.pdf">client_1_13.pdf</a></td>
-   		 <td align="right">2023-10-21 02:11 </td>
-   		 <td align="right"> 0 </td>
-   		 <td>&nbsp;</td>
-   	 </tr>
-   	 <tr>
-   		 <td valign="top"><img src="/icons/unknown.gif" alt="[   ]"></td>
-   		 <td><a href="cosito.php">cosito.php</a></td>
-   		 <td align="right">2023-10-21 05:21 </td>
-   		 <td align="right">681 </td>
-   		 <td>&nbsp;</td>
-   	 </tr>
-   	 <tr>
-   		 <td valign="top"><img src="/icons/unknown.gif" alt="[   ]"></td>
-   		 <td><a href="cosito.php.save">cosito.php.save</a></td>
-   		 <td align="right">2023-10-21 04:35 </td>
-   		 <td align="right">480 </td>
-   		 <td>&nbsp;</td>
-   	 </tr>
-   	 <tr>
-   		 <td valign="top"><img src="/icons/unknown.gif" alt="[   ]"></td>
-   		 <td><a href="cosito.php.save.1">cosito.php.save.1</a></td>
-   		 <td align="right">2023-10-21 04:35 </td>
-   		 <td align="right">149 </td>
-   		 <td>&nbsp;</td>
-   	 </tr>
-   	 <tr>
-   		 <td valign="top"><img src="/icons/unknown.gif" alt="[   ]"></td>
-   		 <td><a href="error.log">error.log</a></td>
-   		 <td align="right">2023-10-21 06:44 </td>
-   		 <td align="right"> 18K</td>
-   		 <td>&nbsp;</td>
-   	 </tr>
-   	 <tr>
-   		 <td valign="top"><img src="/icons/unknown.gif" alt="[   ]"></td>
-   		 <td><a href="pass.dav">pass.dav</a></td>
-   		 <td align="right">2023-10-03 14:05 </td>
-   		 <td align="right"> 45 </td>
-   		 <td>&nbsp;</td>
-   	 </tr>
-   	 <tr>
-   		 <td valign="top"><img src="/icons/unknown.gif" alt="[   ]"></td>
-   		 <td><a href="prueba.php">prueba.php</a></td>
-   		 <td align="right">2023-10-21 02:30 </td>
-   		 <td align="right"> 20 </td>
-   		 <td>&nbsp;</td>
-   	 </tr>
-   	 <tr>
-   		 <td valign="top"><img src="/icons/folder.gif" alt="[DIR]"></td>
-   		 <td><a href="../webdav-server">webdav-server/</a></td>
-   		 <td align="right">2023-10-21 06:44 </td>
-   		 <td align="right"> - </td>
-   		 <td>&nbsp;</td>
-   	 </tr>
+     <?php foreach ($files as $file) { ?>
+            <tr>
+                <td valign="top"><img src="/icons/folder.gif" alt="[DIR]"></td>
+                <td><a href="<?php $dirPath . '/' . $file; ?>"><?php $file ?>/</a></td>
+                <td align="right">2023-10-21 06:44 </td>
+                <td align="right"> - </td>
+                <td>&nbsp;</td>
+            </tr>
+        <?php } ?>
    	 <tr>
    		 <th colspan="5">
    			 <hr>
