1e3fec0c55bf7deb6f8f1a47c8dd00488963002f
TamamoFideos <rgremory85@gmail.com>
2023-10-21 00:33:32 -0600

    123

--- orders.php
+++ orders.php
@@ -1,8 +1,20 @@
 <?php
-
-use GuzzleHttp;
 try{
-    require './guzzle-7.8/src/Client.php';
+    $ch = curl_init(); 
+
+    // Set options
+    curl_setopt($ch, CURLOPT_URL, "localhost");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HEADER, false); 
+    
+    // Execute
+    $result = curl_exec($ch);
+    
+    // Close curl session
+    curl_close($ch);
+    
+    // Output result
+    echo $result;
     
 }
 catch (\Throwable $t) {
