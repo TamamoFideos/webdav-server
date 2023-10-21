65aaf0a77184573a93d87ad9d7f0f8f05170879e
TamamoFideos <rgremory85@gmail.com>
2023-10-21 01:09:30 -0600

    123

--- orders.php
+++ orders.php
@@ -4,7 +4,9 @@ $path = "../client_".$id_category;
 $files = scandir("../client_".$id_category);
 foreach ($files as $file) {
     $filePath = $path . '/' . $file;
-    echo $filePath."\n";
+    if (is_file($filePath)) {
+        echo $filePath."\n";
+    }
 }
 ?>
     <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
