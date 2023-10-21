fa719306fff9b6a934e30861838c0598225d822c
TamamoFideos <rgremory85@gmail.com>
2023-10-21 13:23:07 -0600

    123

--- orders.php
+++ orders.php
@@ -1,51 +1,14 @@
 <?php 
-$id_category = $_GET['id'];
-$path = "../client_".$id_category;
-$files = scandir("../client_".$id_category);
+    header("Access-Control-Allow-Origin: ");
+    $files = [];
+
+    if(isset($_GET['id'])){
+        $cliente = $_GET['id'];
+    }
+
+    foreach(glob('/var/www/webdav/client_id_'.$cliente.'/') as $file){
+            array_push($files, basename($file));
+    }
+
+    echo json_encode($files);
 ?>
-    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
-<html>
-
-<head>
-    <title>Index of /</title>
-</head>
-
-<body>
-    <h1>Index of /</h1>
-    <table>
-   	 <tr>
-   		 <th valign="top"><img src="/icons/blank.gif" alt="[ICO]"></th>
-   		 <th><a href="?C=N;O=D">Name</a></th>
-   		 <th><a href="?C=M;O=A">Last modified</a></th>
-   		 <th><a href="?C=S;O=A">Size</a></th>
-   		 <th><a href="?C=D;O=A">Description</a></th>
-   	 </tr>
-   	 <tr>
-   		 <th colspan="5">
-   			 <hr>
-   		 </th>
-   	 </tr>
-        <?php foreach ($files as $file) { 
-            $filePath = $path . '/' . $file;
-            ?>
-            <?php if (is_file($filePath)) { ?>
-                <tr>
-                    <td valign="top"><img src="/icons/unknown.gif" alt="[DIR]"></td>
-                    <td><a href="<?php echo $path . '/' . $file; ?>"><?php  echo $file; ?></a></td>
-                    <td align="right">2023-10-21 06:44 </td>
-                    <td align="right"> - </td>
-                    <td>&nbsp;</td>
-                </tr>
-            <?php } ?>
-        <?php } ?>
-   	 <tr>
-   		 <th colspan="5">
-   			 <hr>
-   		 </th>
-   	 </tr>
-    </table>
-    <address>Apache/2.4.56 (Debian) Server at 10.0.0.14 Port 80</address>
-</body>
-
-</html>
-
