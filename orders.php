6148aa28f702bdcf2777258008baed087edda0e0
TamamoFideos <rgremory85@gmail.com>
2023-10-21 00:09:16 -0600

    123

--- orders.php
+++ orders.php
@@ -3,10 +3,7 @@
 use GuzzleHttp;
 try{
     require './guzzle-7.8/src/Client.php';
-    $client = new Client();
-    $response = $client->request('GET', 'localhost');
-    echo $response->getStatusCode(); // 200
-    echo $response->getBody();
+    
 }
 catch (\Throwable $t) {
     echo "caught!\n";
